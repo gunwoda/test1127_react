ac0e0d7e6b21b19fe236835c47b6c2f226f741a1
gunwoda <gunwo3442@gmail.com>
2024-11-27 17:01:48 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #f9f9f9;
+  font-family: 'Arial', sans-serif;
+  color: #333;
+`;
+
+const Header = styled.h1`
+  font-size: 2.5rem;
+  margin-bottom: 2rem;
+  color: #007BFF;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  width: 80%;
+  max-width: 600px;
+`;
+
+const ListItem = styled.li`
+  background: #fff;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  margin-bottom: 1rem;
+  padding: 1rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s;
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+`;
 
 function App() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/findall') // public 폴더에 data.json 파일 위치
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching the JSON file:', error);
+        setError('Failed to load data.');
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>데이터 출력</Header>
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <List>
+          {data.map((item, index) => (
+            <ListItem key={index}>
+              <strong>UserName : {item.username}</strong>
+              <p>Email : {item.email}</p>
+              <p>Phone Number : {item.phonenumber}</p>
+              <p>PassWord: {item.password}</p>
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </Container>
   );
 }
 
